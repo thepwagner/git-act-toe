f22ca94ebb50944705a07ced279714bd9778f2b5
Peter Wagner <git@mycloudand.me>
2018-12-09 13:36:23 -0500

    push from desktop

--- action.py
+++ action.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python
 
 import re
+
+from git import Repo
 from xo.board import Board
+from xo.game import Game
 
 place_x = re.compile(' *[xX] *(\\||$)')
 place_o = re.compile(' *[oO] *(\\||$)')
 empty = re.compile(' +(\\||$)')
 padding = re.compile('[^\\.xo]*')
 
-def load_board(fn):
-    with open('game.txt') as game_txt:
-        board_data = ''
-        line = game_txt.readline()
-        while line:
-            line = place_x.sub('x', line)
-            line = place_o.sub('o', line)
-            line = empty.sub('.', line)
-            line = padding.sub('', line)
-            board_data += line.strip()
-            line = game_txt.readline()
-        return Board.fromstring(board_data)
+def load_board(game_in):
+    board_data = ''
+    for line in game_in.decode('ascii').split('\n'):
+        line = place_x.sub('x', line)
+        line = place_o.sub('o', line)
+        line = empty.sub('.', line)
+        line = padding.sub('', line)
+        board_data += line.strip()
+    return Board.fromstring(board_data)
+
 
 if __name__ == '__main__':
-   b = load_board('game.txt')
-   print(b.toascii())
+    # Load board states from current and previous commits:
+    r = Repo()
+    last_board = load_board((r.commit('HEAD~1').tree / 'game.txt').data_stream.read())
+    cur_board = load_board((r.head.commit.tree / 'game.txt').data_stream.read())
+
+    # Reload game from last board:
+    g = Game()
+    g.start('x')
+    g.board = last_board
+
+    # Find the move:
+    for i, p in enumerate(cur_board.cells):
+        if last_board.cells[i] != p:
+            col = i % 3
+            row = int((i - col) / 3)
+            e = g.moveto(row+1, col+1)
+            print(e)
+
+    print(cur_board.toascii())
+    print(last_board.toascii())
